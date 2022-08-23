5037aa04e16987e41fc364d1e32eb26aa032fea0
KrivovDE <55reigen55@mail.ru>
2022-08-23 14:44:20 +0300

    SRP

---
+++ SRP.py
@@ -0,0 +1,35 @@
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.count += 1
+        self.entries.append(f'{self.count}: {text}')
+
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        return '\n'.join(self.entries)
+
+
+# Добавлять подобные методы в вышеописанный класс - плохая идея
+    # def save(self, filename):
+    #     file = open(filename, 'w')
+    #     file.write(str(self))
+    #     file.close()
+    #
+    # def load(self, filename):
+    #     pass
+    #
+    # def low_from_web(self, uri):
+    #     pass
+
+# Вот такая реализация позволит соблюсти SRP
+class PersistenceManager:
+    @staticmethod
+    def save_to_file(journal, filename):
+        file = open(filename, 'w')
+        file.write(str(journal))
+        file.close()
