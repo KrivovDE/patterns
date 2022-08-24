dda45cb91e32cb2b122bb5554ceeb2c44c2e3b27
KrivovDE <55reigen55@mail.ru>
2022-08-24 22:47:10 +0300

    OSP2

--- OSP.py
+++ OSP.py
@@ -37,3 +37,68 @@ class ProductFilter:
 # это прямое нарушение ОСР, функциональность должна добавляться
 # через расширение, а не через модификацию
 
+
+#Применен корпоративный шаблон Specification
+
+class Specification:
+    """
+    Класс проверяет удовлетворяет ли конкретный элимент определенному критерию
+    """
+    def is_satisfied(self, item):
+        pass
+
+
+class Filter:
+    def filter(self, items, spec):
+        pass
+
+
+class ColorSpecification(Specification):
+    """
+    Фильтер продуктов по цвету
+    """
+    def __init__(self, color):
+        self.color = color
+
+    def is_satisfied(self, item):
+        return item.color == self.color
+
+
+class SizeSpecification(Specification):
+    """
+    Фильтер продуктов по размеру
+    """
+    def __init__(self, size):
+        self.size = size
+
+    def is_satisfied(self, item):
+        return item.size == self.size
+
+
+class BetterFilter(Filter):
+    def filter(self, items, spec):
+        for item in items:
+            if spec.is_satisfied(item):
+                yield item
+
+
+apple = Product('Apple', Color.GREEN, Size.SMALL)
+tree = Product('Tree', Color.GREEN, Size.LARGE)
+house = Product('House', Color.BLUE, Size.LARGE)
+
+# создаем список продуктов по которым проходимся циклом
+products = [apple, tree, house]
+
+# Ищем все зеленые продукты
+# фильтр который создан с помощью шаблона спецификации
+bf = BetterFilter()
+# Создаем спицификакцию
+green = ColorSpecification(Color.GREEN)
+
+for p in bf.filter(products, green):
+    print(f'{p.name}  - точно зеленый')
+
+
+
+
+
